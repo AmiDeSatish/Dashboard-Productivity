8a4c4c9485f53006eeace9a3a0f69d5da66aefd3
LeRoiDuRERb <tbone_92@outlook.fr>
2025-12-17 12:34:04 +0100

    Sidebar advancement 1

--- src/pages/Home/Sidebar/Sidebar.tsx
+++ src/pages/Home/Sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
 import {useState} from "react"
 import profilePic from "../../../assets/profilpic.jfif"
+import dashboardIcon from "./dashboard.png"
+import inboxIcon from "./inbox.png"
 import style from "./Sidebar.module.css"
 
 function SideBar(){
@@ -14,6 +16,22 @@ function SideBar(){
           <div className={style.profilInfo}>
             <p className={style.profilUserName}>{username}</p>
             <p className={style.profilEmail}>{email}</p>
+          </div>
+        </div>
+        <div className={style.dashboardInboxContainer}>
+          <div className={style.dashboardContainer}>
+            <div className={style.dashboardLeft}>
+              <img className={style.dashboardIcon} src={dashboardIcon}/>
+              <p>Dashboard</p>
+            </div>
+            <div className={style.dashboardNotif}></div>
+          </div>
+          <div className={style.inboxContainer}>
+            <div className={style.inboxLeft}>
+              <img className={style.inboxIcon} src={inboxIcon}/>
+              <p>Inbox</p>
+            </div>
+            <div className={style.inboxNotif}></div>
           </div>
         </div>
       </div>
